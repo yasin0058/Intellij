514f099e0bd93b5bf0a129d634c439cccefa301c
YASIN OZTURK <yasinozturk0058@gmail.com>
2023-08-14 18:37:38 +0300

    version 1

--- src/githubtry/CLASS1.java
+++ src/githubtry/CLASS1.java
@@ -3,5 +3,6 @@ package githubtry;
 public class CLASS1 {
     public static void main(String[] args) {
         System.out.println("Version 1");
-    }
+        System.out.println("vVERSİON 2");
+        }
 }
